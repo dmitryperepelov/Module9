8025bdf725fdb0972b1de4708c554699985ce8cc
Dmitry Perepelov <d.perepelov999@gmail.com>
2022-07-28 21:44:29 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var exceptions = new Exception[] { new MyException(), new ArgumentOutOfRangeException(), new StackOverflowException(), new ArgumentNullException(), new AccessViolationException() };
+            foreach (var item in exceptions)
+            {
+                try
+                {
+                    throw new Exception(item.GetType().ToString());
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class MyException : Exception
+    {
+
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static List<string> list = new List<string> { "Иванов", "Петров", "Сидоров", "Федоров", "Макаров"};
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите число 1 или 2: ");
+                var number = Console.ReadLine();
+                if (number != "1" && number != "2")
+                    throw new MyException("Необходимо указать число 1 или 2!");
+
+                var sort = new SortClass(list);
+
+                sort.SortAscending += SortAscending;
+                sort.SortDescending += SortDescending;
+                var sortedItems = sort.Sort(number);
+                foreach (string s in sortedItems)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            catch(MyException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+        static List<string> SortAscending(List<string> items)
+        {
+            return items.OrderBy(i => i).ToList();
+        }
+
+        static List<string> SortDescending(List<string> items)
+        {
+            return items.OrderByDescending(i => i).ToList();
+        }
+    }
+
+    public delegate List<string> Notify(List<string> items);
+
+    public class SortClass
+    {
+        public SortClass(List<string> items)
+        {
+            this.items = items;
+        }
+
+        private List<string> items;
+
+        public event Notify SortAscending;
+
+        public event Notify SortDescending;
+
+        public List<string> Sort(string order)
+        {
+            Console.WriteLine($"Сортировка по {(order == "1" ? "возрастанию" : "убыванию")}");
+            switch (order)
+            {
+                case "1":
+                    return SortAscending?.Invoke(items);
+                case "2":
+                    return SortDescending?.Invoke(items);
+            }
+            return null;
+        }
+    }
+
+    public class MyException : Exception 
+    {
+        public MyException(string message) : base(message) { }
+    }
+}
